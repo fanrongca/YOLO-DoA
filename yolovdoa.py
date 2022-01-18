3ded0c90f6bd1973e64c79be8c1ee2e41e85bf11
fanrongca <46212148+fanrongca@users.noreply.github.com>
2022-01-18 21:34:27 +0800

    Update yolovdoa.py

--- yolovdoa.py
+++ yolovdoa.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding=utf-8
 #================================================================
-#   Copyright (C) 2019 * Ltd. All rights reserved.
+#   Copyright (C) 2021 * Ltd. All rights reserved.
 #
 #   Editor      : PyCharm
 #   File name   : yolovdoa.py
